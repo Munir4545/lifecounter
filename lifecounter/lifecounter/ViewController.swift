99705ef1d989709157d82490babe8b1b4ad3daa1
munir45 <memamy3@uw.edu>
2025-04-21 19:22:59 -0700

    constraints

--- lifecounter/lifecounter/ViewController.swift
+++ lifecounter/lifecounter/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var Player1Life: UILabel!
     @IBOutlet weak var Player2Life: UILabel!
+    @IBOutlet weak var loserLabel: UILabel!
     
     var Player1 : Int = 20
     var Player2 : Int = 20
@@ -17,45 +18,64 @@ class ViewController: UIViewController {
     @IBAction func player1AddOne(_ sender: Any) {
         self.Player1 += 1
         Player1Life.text = String(self.Player1)
+        checkLoserStatus()
     }
     
     @IBAction func player1AddFive(_ sender: Any) {
         self.Player1 += 5
         Player1Life.text = String(self.Player1)
+        checkLoserStatus()
     }
     
     @IBAction func player1RemoveOne(_ sender: Any) {
         self.Player1 -= 1
         Player1Life.text = String(self.Player1)
+        checkLoserStatus()
     }
     
     @IBAction func player1RemoveFive(_ sender: Any) {
         self.Player1 -= 5
         Player1Life.text = String(self.Player1)
+        checkLoserStatus()
     }
     
     @IBAction func player2AddOne(_ sender: Any) {
         self.Player2 += 1
         Player2Life.text = String(self.Player2)
+        checkLoserStatus()
     }
     
     @IBAction func player2AddFive(_ sender: Any) {
         self.Player2 += 5
         Player2Life.text = String(self.Player2)
+        checkLoserStatus()
     }
     
     @IBAction func player2RemoveOne(_ sender: Any) {
         self.Player2 -= 1
         Player2Life.text = String(self.Player2)
+        checkLoserStatus()
     }
     
     @IBAction func player2RemoveFive(_ sender: Any) {
         self.Player2 -= 5
         Player2Life.text = String(self.Player2)
+        checkLoserStatus()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        checkLoserStatus()
+    }
+    
+    func checkLoserStatus() {
+        if Player1 < 1 {
+            loserLabel.text = "Player 1 Loses"
+            loserLabel.isHidden = false
+        } else if Player2 < 0 {
+            loserLabel.text = "Player 2 Loses"
+            loserLabel.isHidden = false
+        }
     }
 
 
